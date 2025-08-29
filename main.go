05482f24ffdb1eec0cd40be3eac5bcfbd3472e62
joseph0x45 <zozozozeph@gmail.com>
2025-08-29 23:55:31 +0000

    remove magic numbers

--- main.go
+++ main.go
@@ -6,19 +6,21 @@ import (
 	"time"
 )
 
+const PORT = ":6969"
+
 func main() {
 	mux := http.NewServeMux()
 
   mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	server := &http.Server{
-		Addr:         ":6969",
+		Addr:         PORT,
 		Handler:      mux,
 		ReadTimeout:  time.Minute,
 		WriteTimeout: time.Minute,
 	}
 
-	log.Println("[INFO]: Server listening on port 8080")
+	log.Println("[INFO]: Server listening on port ", PORT)
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
